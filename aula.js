f209e280612038ec7d2abf79cdfb9cb02fd9df62
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:49:58 -0300

    Modificando arquivo JavaScript

--- aula.js
+++ aula.js
@@ -15,3 +15,11 @@ console.log("Olá, tudo bem?");
 // criando nossa primeira variavel
 let mensagem = "Olá, Mundo!";
 console.log("Esse console é da variavel:", mensagem);
+
+// criar uma variavel nome
+let nome = "Beatriz";
+console.log("Essa é a variavel nome:", nome);
+
+// criar uma variavel sobrenome
+let sobrenome = "Dantas";
+console.log("Essa é a variavel sobrenome:", sobrenome);
