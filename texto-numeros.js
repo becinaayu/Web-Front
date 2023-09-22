7926bf5e0948a0f9861173927c01d99de9dc2feb
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:15:38 -0300

    Adicionando texto

---
+++ texto-numeros.js
@@ -0,0 +1,17 @@
+// parseInt - Pega os numeros inteiros dos numeros flutuantes
+// parseFloat(numString) - Converteu a string para um número
+// typeof - Diz a definição da variavel
+// length - Serve para contar os caracteres de uma variavel
+// indexOf("") - Irá buscar a primeira posição do que foi pedido
+// slice(0,..) - Mostra o valor determinado que foi escolhido
+// Atalho para comentar - Ctrl + K + C
+let numFloat = 123.5422;
+let numString = String(numFloat);
+let verificar = typeof numString;
+
+let nomeAluno1 = "Pedro Henrique";
+
+let txt = "Estão chegando as provas";
+
+// console.log(txt.slice(0,5));
+console.log(txt.substr(6,8));
