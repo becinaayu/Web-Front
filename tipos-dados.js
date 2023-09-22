a7ae0177585b9f84c4826f23f380001dad72eab6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 08:50:44 -0300

    Adicionando outros tipos de dados

---
+++ tipos-dados.js
@@ -0,0 +1,18 @@
+// Não começar as variaveis com letra maiuscula
+
+// Numeros inteiros;
+let idadeAluno = 18;
+
+// Numeros flutuantes com casas decimais;
+let valorProduto = 5.35;
+
+// Variavel de texto, string;
+let nomeDoAluno = "Beatriz"
+
+// Booleanos - Recebe valor true ou false
+let casado = true;
+
+// Array - Conjunto de elementos
+let linguagens = ["Java", "JavaScipt", "C#"];
+//                   0        1         2
+console.log(linguagens[1])
