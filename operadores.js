5ac547c9f55530cfb17cff6b99de722b4c4c8046
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:34:14 -0300

    Adicionando operacoes

---
+++ operadores.js
@@ -0,0 +1,15 @@
+// Declaração de variaveis
+let num1 = 10;
+let num2 = 5;
+
+// Operações matematicas com as variaveis
+let soma = num1+num2;
+let subtracao = num1-num2;
+let multiplicacao = num1*num2;
+let divisao = num1/num2;
+
+// Exibindo resultados no console
+console.log("O resultado da soma é:", soma);
+console.log("O resultado da subtração é:", subtracao);
+console.log("O resultado da multiplicação é:", multiplicacao);
+console.log("O resultado da divisão é:", divisao);
