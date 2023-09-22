c98fcff517be9d4e94017d03768b128a47aae7b4
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:25:34 -0300

    Adicionando manipulacoes de texto

---
+++ manipulando-texto.js
@@ -0,0 +1,10 @@
+// replace - Irá trocar as palavras
+
+let txt = "Estão chegando as provas";
+let num = 123.5432;
+
+
+// console.log(txt.replace("provas", "avaliações"));
+// console.log (txt.toUpperCase());
+// console.log(num.toFixed(0)); - Remove casas decimais
+
