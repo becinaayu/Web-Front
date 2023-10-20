8d29213d8bf49354269fb36147038a32282cd4da
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 09:48:52 -0300

    Exemplo de array

---
+++ array-exemplo.js
@@ -0,0 +1,7 @@
+const times = ["Palmeiras", "Flamengo", "Botafogo"];
+
+// push = adicionar elementos em uma array.
+times.push("Corinthians")
+
+// console.table = mostrar em forma de tabela.
+console.table(times)
