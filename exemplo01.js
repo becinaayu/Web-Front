5a54159e70b0285fa144ce7561fef18a8713fb89
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 08:34:22 -0300

    Adicionando um novo arquivo de incremento

---
+++ exemplo01.js
@@ -0,0 +1,18 @@
+let valor = 7;
+
+console.log(valor);
+
+// Adicionando valor.
+valor = valor + 1; 
+
+console.log(valor);
+
+// Adicionando valor de outra forma.
+valor ++;
+
+console.log(valor);
+
+// Adionando um valor de uma terceira forma.
+valor += 2;
+
+console.log(valor);
