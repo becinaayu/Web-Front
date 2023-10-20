609431719ce3e65ebded8b1ab528292f9134b2d6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 08:47:27 -0300

    Adicionando novas formas de Decrementar e fazer contas

---
+++ exemplo02.js
@@ -0,0 +1,20 @@
+
+// Forma mais "avaçada"
+
+let valor = 7;
+console.log(valor);
+
+// Dimininuindo o valor.
+valor --;
+console.log(valor);
+
+// Dimininuindo o valor mais de um.
+valor -= 2;
+console.log(valor);
+
+// Efetuando contas.
+valor *= 2;
+console.log("multiplicação por 2 =", valor);
+
+valor /= 2;
+console.log("divisão por 2 =", valor);
