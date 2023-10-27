74bb81c440abed91f4e2ff63dd1c62e91dfa39cb
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 09:58:24 -0300

    Adicionando projeto com CSS, HTML e DOM

--- js/dom.js
+++ js/dom.js
@@ -1,13 +1,35 @@
-function teste(arg1, arg2) {
-    console.log(arg1 + arg2);
-}
+// Função de somar:
 
-teste(7, 1);
+// function teste(arg1, arg2) {
+//     console.log(arg1 + arg2);
+// }
+// teste(7, 1);
 
-function imprimenome(name) {
-    console.log(`Meu nome é ${name}`);
-}
-imprimenome("Beatriz");
-imprimenome("Taiki");
-imprimenome("Dantas");
+// Função de impressão:
 
+// function imprimenome(name) {
+//     console.log(`Meu nome é ${name}`);
+// }
+// imprimenome("Beatriz");
+// imprimenome("Taiki");
+// imprimenome("Dantas");
+
+// Função de botão com clique:
+
+// function cliqueAqui() {
+//     alert("Olá");
+// }
+
+// Criando Variavel:
+
+const tirarDiv = document.querySelector(".tirarDiv");   // DOM
+const voltarDiv = document.querySelector(".voltarDiv");
+const card = document.querySelector(".card");
+
+tirarDiv.addEventListener("click", function(){
+    card.style.display = "none";
+})
+
+voltarDiv.addEventListener("click", function(){
+    card.style.display = "block";
+})
